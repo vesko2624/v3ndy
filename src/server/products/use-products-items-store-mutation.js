505df571efc75e86be7c6a5092005b60c0fc8ac3
vgeorgiev <vgeorgiev@2create.studio>
2023-08-08 00:21:05 +0300

    Set max items per product to 15

--- src/server/products/use-products-items-store-mutation.js
+++ src/server/products/use-products-items-store-mutation.js
@@ -3,13 +3,17 @@
  */
 import useServerMutation from "@/server/use-server-mutation.js";
 
+const MAX_ITEMS_PER_PRODUCT = 15;
+
 const useProductsItemsStoreMutation = (product) => {
     return useServerMutation((store) => {
         const serverProduct = store.findProduct(product.id);
 
         return store.updateProduct({
             ...serverProduct,
-            quantity: product.quantity + 1,
+            quantity: product.quantity >= MAX_ITEMS_PER_PRODUCT
+              ? MAX_ITEMS_PER_PRODUCT
+              : product.quantity + 1,
         });
     }, (store) => ({
         findProduct: store.findProduct,
