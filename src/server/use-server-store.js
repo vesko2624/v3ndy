86692d1aa8088a0158bbcd2393f02ebce597184f
vgeorgiev <vgeorgiev@2create.studio>
2023-08-07 23:23:04 +0300

    Persist store

--- src/server/use-server-store.js
+++ src/server/use-server-store.js
@@ -3,32 +3,39 @@
  */
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
+import { createJSONStorage, persist } from "zustand/middleware";
 
 const useServerStore = create(
-  immer((set, state) => ({
-      products: [],
-      addProduct: (product) => {
-          set((state) => {
-              state.products = [...state.products, product];
-          });
-      },
-      updateProduct: (product) => {
-          set((state) => {
-              state.products = state.products.map((item) => {
-                  if (item.id === product.id) {
-                      return { ...product };
-                  }
+  persist(
+      immer((set, state) => ({
+          products: [],
+          addProduct: (product) => {
+              set((state) => {
+                  state.products = [...state.products, product];
+              });
+          },
+          updateProduct: (product) => {
+              set((state) => {
+                  state.products = state.products.map((item) => {
+                      if (item.id === product.id) {
+                          return { ...product };
+                      }
 
-                  return item;
-              })
-          });
-      },
-      findProduct: (id) => state().products.find((product) => product.id === id),
-      removeProduct: (id) => {
-          set((state) => {
-              state.products = state.products.filter((product) => product.id !== id);
-          });
-      },
-  }))
+                      return item;
+                  })
+              });
+          },
+          findProduct: (id) => state().products.find((product) => product.id === id),
+          removeProduct: (id) => {
+              set((state) => {
+                  state.products = state.products.filter((product) => product.id !== id);
+              });
+          },
+      })),
+    {
+        name: '@server',
+        storage: createJSONStorage(() => localStorage),
+    }
+  )
 );
 export default useServerStore;
